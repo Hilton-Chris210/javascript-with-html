3ede174ff6fef9f7d3d964d9f58673685748004b
Chris Hilton <hilton.chris210@gmail.com>
2022-11-21 11:45:25 -0600

    Added inline and external javascript

---
+++ js/external.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('Hello from external JavaScript');
+alert('Welcome To My Website!');
+var userInput = prompt('What is your favorite color?:');
+alert('Your favorite color is: ' + userInput);
